4a2d16365224df8e96c746ffc65823ebac22023a
sushma1261 <39758057+sushma1261@users.noreply.github.com>
2024-10-18 18:01:27 -0700

    Initial Commit

---
+++ MockItUp/MockItUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  MockItUpApp.swift
+//  MockItUp
+//
+//  Created by Sushma Manthena on 18/10/24.
+//
+
+import SwiftUI
+
+@main
+struct MockItUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
