c0258b0d99c232257b4681207ffab72fee300191
sushma1261 <39758057+sushma1261@users.noreply.github.com>
2024-10-26 20:56:29 -0700

    Storing user preferences

--- MockItUp/Views/PreferrencesView.swift
+++ MockItUp/Views/PreferrencesView.swift
@@ -8,49 +8,45 @@
 import SwiftUI
 
 struct PreferrencesView: View {
-    
-    
-//    @EnvironmentObject var preferencesViewModel: PreferencesViewModel
 
-//    @State private var selectedLevel: Levels = .easy
-    @State var jobType: String = ""
-    @State var level: String = "asd"
+    @State var level: String = ""
     @State var yoe: String = ""
     @State var companyName: String = ""
     @State var jobDescription: String = ""
     
-    @StateObject var preference: PreferencesViewModel = PreferencesViewModel()
+    @ObservedObject var preference: PreferencesViewModel = PreferencesViewModel()
+    
     
     var body: some View {
         VStack {
             HStack {
-                TextField("Job Type", text: $preference.level)
+                TextField("Job Type", text: $preference.preferenceModal.jobType)
                     .padding(.horizontal)
                     .frame(height: 55)
                     .background(Color(UIColor.secondarySystemBackground))
                     .cornerRadius(10)
                 
-                TextField("Level", text: $level)
+                TextField("Level", text: $preference.preferenceModal.level)
                     .padding(.horizontal)
                     .frame(height: 55)
                     .background(Color(UIColor.secondarySystemBackground))
                     .cornerRadius(10)
             }
 
-            TextField("Years of Experience", text: $yoe)
+            TextField("Years of Experience", text: $preference.preferenceModal.yoe)
                 .keyboardType(.decimalPad)
                 .padding(.horizontal)
                 .frame(height: 55)
                 .background(Color(UIColor.secondarySystemBackground))
                 .cornerRadius(10)
             
-            TextField("Company Name", text: $companyName)
+            TextField("Company Name", text: $preference.preferenceModal.companyName)
                 .padding(.horizontal)
                 .frame(height: 55)
                 .background(Color(UIColor.secondarySystemBackground))
                 .cornerRadius(10)
             
-            TextField("Job Description", text: $jobDescription)
+            TextField("Job Description", text: $preference.preferenceModal.jobDescription)
                 .padding(.horizontal)
                 .frame(height: 150)
                 .background(Color(UIColor.secondarySystemBackground))
